517f59daa0456e75df7fc5b895326795b48f899a
Tarique12344 <Tariqueelshamy11@gmail.com>
2023-11-27 05:32:25 -0500

    cont HW

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,43 +1,73 @@
-const User = require('../models/User')
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
 
 const handleErrors = (err) => {
-    console.log(err.message, err.code)
+    console.log(err.message, err.code);
     let errors = {email: '', password: ''};
 
-    // if(err.code == 11000){
-    //     errors.email = "Email is already in use";
-    //     return errors;
-    // }
+    //incorrect email
+    if(err.message === "incorrect email"){
+        errors.email = 'Email is not registered';
+    };
+
+    //incorrect password
+    if(err.message === 'incorrect password'){
+        errors.password = 'Password is incorrect';
+    };
+
+    if(err.code == 11000){
+        errors.email = "Email is already in use";
+        return errors;
+    }
 
     if(err.message.includes('user validation failed')){
         Object.values(err.errors).forEach(({properties}) => {
             errors[properties.path] = properties.message
-        }); 
+        });
     }
-    return errors;   
+    return errors;
+
 }
 
-module.exports.signup_get = (req , res) => {
+const maxAge= 3 * 24 * 60 * 60;
+const createToken = (id) => {
+    return jwt.sign({ id }, 'secret message', {
+        expiresIn: maxAge
+    })
+}
+
+module.exports.signup_get = (req, res) => {
     res.render('signup');
 }
-module.exports.login_get = (req , res) => {
+
+module.exports.login_get = (req, res) => {
     res.render('login');
 }
+
 module.exports.signup_post = async (req, res) => {
     const { email, password } = req.body;
     try {
         const user = await User.create({ email, password });
-        res.status(201).json(user);
+        const token = createToken(user._is);
+        res.cookie('jwt', token , {httpOnly: true, maxAGe: maxAge * 1000});
+        res.status(201).json({ user: user._id });
     } catch (err) {
-       const errors = handleErrors(err)
+        const errors = handleErrors(err);
         res.status(400).send({ errors });
     }
-
-
 }
-module.exports.login_post = async (req , res) => {
+
+module.exports.login_post = async (req, res) => {
     const { email, password } = req.body;
 
-    console.log(email, password);
-    res.send('user login');
+   try {
+    const user = await User.login(email, password);
+    const token = createToken(user._is);
+    res.cookie('jwt', token , {httpOnly: true, maxAGe: maxAge * 1000});
+    res.status(200).json({user: user._id});
+
+   } catch(err){
+    const errors = handleErrors(err);
+    res.status(400).json({ errors })
+   }
 }
--- models/User.js
+++ models/User.js
@@ -1,20 +1,45 @@
 const mongoose = require('mongoose');
-const { isEmail } = require('validator')
+const { isEmail } = require('validator');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     email: {
         type: String,
-        required: [true, 'Please enter an email'],
+        required: [ true, 'Please enter an email' ],
         unique: true,
         lowercase: true,
-        validate: [isEmail, "please enter a valid email"]
+        validate: [ isEmail, 'Please enter a valid email' ]
     },
     password: {
         type: String,
-        required: [true, 'please enter a password'],
-        minLength: [6, "Minimum length is 6 characters"],
+        required: [ true, 'Please enter a password' ],
+        minlength: [6, 'Minimum length is 6 characters']
+    }
+});
+//fire a function after document is saved to the database
+// userSchema.post('save', function(doc, next) {
+//     console.log('new user was created saved', doc);
+//     next();
+// });
+
+userSchema.pre('save', async function(next) {
+    // console.log('user about to be created and saved', this);
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
+//static method to login the user
+userSchema.statics.login = async function(email, password) {
+    const user = await this.findOne({ email });
+    if(user){
+        const auth = await bcrypt.compare(password, user.password);
+        if(auth){
+            return user;
+        }
+        throw Error('incorrect password');
+    }
+    throw Error('incorrect email');
 }
-})
 
 const User = mongoose.model('user', userSchema);
 
